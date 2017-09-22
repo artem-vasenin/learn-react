161805e35c806a4d96762f40f7b4738ea9ab38de
artem-vasenin <artem.vasenin@accenture.com>
2017-09-22 16:55:22 +0300

    Попытка не увенчавшаяся успехом...

--- src/index.js
+++ src/index.js
@@ -34,28 +34,62 @@ let Addform = React.createClass({
 });
 
 let Booklist = React.createClass({
+  getInitialState: function(){
+    return { edit: false };
+  },
   edit: function(){
-    console.log('Редактирование');
+    this.setState({edit: true})
+  },
+  save: function(){
+    let author = this.refs.author.value;
+    let title = this.refs.title.value;
+    let year = this.refs.year.value;
+    let pages = this.refs.pages.value;
+    let i = this.props.index;
+    this.setState({edit: false});
+    // this.props.updateBook({author: author, title: title, year: year, pages: pages}, this.props.index);
+    let arr = this.state.data;
+    arr[i] = {author: author, title: title, year: year, pages: pages};
+    this.setState({data: arr});
   },
   delete: function(){
-    console.log('Удаление');
+    // this.props.deleteBook(this.props.index);
+    let i = this.props.index;
+    let arr = this.state.data;
+    arr.splice(i, 1);
+    this.setState({data: arr});
   },
   render: function(){
-    var data = this.props.data;
-    var booksListTemplate;
+    let data = this.props.data;
+    let booksListTemplate;
     if(data.length){
       booksListTemplate = data.map(function(item, index){
-        return (
-          <li className='list__item' key={index}>
-            <i className='list__index'>{index}</i>
-            {item.author} <b>"{item.title}"</b> - {item.year}г ({item.pages} стр)
+        if(this.state.edit){
+          return (
+            <li className='list__item' key={index} index={index} updateBook={this.updateBook} deleteBook={this.deleteBook}>
+              <i className='list__index'>{index + 1}</i>
+              {item.author} <b>"{item.title}"</b> - {item.year}г ({item.pages} стр)
+              <span className='list__buttons'>
+                <button onClick={this.edit} className='button button--green'>Редактировать</button>
+                <button onClick={this.delete} className='button button--red'>Удалить</button>
+              </span>
+            </li>
+          );
+        }else{
+          return (
+          <li className='list__item' key={index} index={index} updateBook={this.updateBook} deleteBook={this.deleteBook}>
+            <i className='list__index'>{index + 1}</i>
+            <input ref='author' className='editfield editfield--author' type='text' defaultValue={item.author} />
+            <input ref='title' className='editfield editfield--title' type='text' defaultValue={item.title} /> -
+            <input ref='year' className='editfield editfield--year' type='text' defaultValue={item.year} />г
+            (<input ref='pages' className='editfield editfield--pages' type='text' defaultValue={item.pages} /> стр)
             <span className='list__buttons'>
-              <button onClick={this.edit} className='button button--green'>Редактировать</button>
-              <button onClick={this.del} className='button button--red'>Удалить</button>
+              <button onClick={this.save} className='button'>Сохранить</button>
             </span>
           </li>
-        );
-      });
+          );
+        }
+      },this);
     }else{
       booksListTemplate = <li className='list__item'>Библиотека пока пуста.</li>
     }
@@ -74,6 +108,16 @@ let Booklist = React.createClass({
 });
 
 let Library = React.createClass({
+  // deleteBook: function(i){
+  //   let arr = this.state.data;
+  //   arr.splice(i, 1);
+  //   this.setState({data: arr});
+  // },
+  // updateBook: function(args, i){
+  //   let arr = this.state.data;
+  //   arr[i] = args;
+  //   this.setState({data: arr});
+  // },
   render: function(){
     var books = [
       {author: 'Жуковский', title: 'Баллады', year: 2014, pages: 40},
