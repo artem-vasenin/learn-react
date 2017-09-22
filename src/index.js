ae68fee859c5a0a43867efdcddb98cb761e42cb4
artem-vasenin <artem.vasenin@accenture.com>
2017-09-22 19:00:14 +0300

    Добрался до кнопок Редактировать-удалить

--- src/index.js
+++ src/index.js
@@ -5,36 +5,47 @@ import './css/style.css';
 
 const container = document.getElementById('root');
 
-let Library = React.createClass({
+let data = [
+	{author: 'Крапивин', 	title: 'Голубятня на жёлтов поляне', 	year: 2000, pages: 400},
+	{author: 'Дяченко', 	title: 'Vite Nostra', 					year: 2007, pages: 450},
+	{author: 'Олди', 		title: 'Герой должен быть один', 		year: 2000, pages: 560},
+	{author: 'Стругацкие', 	title: 'Трудно быть богом', 			year: 1963, pages: 760},
+	{author: 'Семёнова', 	title: 'Волкодав', 						year: 2014, pages: 40},
+	{author: 'Пехов', 		title: 'Искры и ветер', 				year: 2007, pages: 450},
+	{author: 'Орлов', 		title: 'Тени войны', 					year: 2000, pages: 560},
+	{author: 'Перумов', 	title: 'Череп на рукаве', 				year: 1963, pages: 760}
+];
+
+const Library = React.createClass({
 	render: function(){
 		return (
 			<div className='library'>
 				<h1>Библиотека</h1>
-				<Addform />
-				<BookList />
+				<AddForm />
+				<BookList books={data} />
 			</div>
 		);
 	}
 });
 
-let Addform = React.createClass({
+const AddForm = React.createClass({
 	render: function(){
 		return (
 			<div className='add-form'>
 				<label className='label'>
-					<span className='label__text'>Заголовок</span>
+					<span className='label__text'>Заголовок:</span>
 					<input className='textfield textfield--title' />
 				</label>
 				<label className='label'>
-					<span className='label__text'>Автор</span>
+					<span className='label__text'>Автор:</span>
 					<input className='textfield textfield--author' />
 				</label>
 				<label className='label'>
-					<span className='label__text'>Дата печати</span>
+					<span className='label__text'>Дата:</span>
 					<input className='textfield textfield--date' />
 				</label>
 				<label className='label'>
-					<span className='label__text'>Количество страниц</span>
+					<span className='label__text'>Страниц:</span>
 					<input className='textfield textfield--pages' />
 				</label>
 				<div className='button-block'>
@@ -45,21 +56,29 @@ let Addform = React.createClass({
 	}
 });
 
-let BookList = React.createClass({
+const BookList = React.createClass({
 	render: function(){
+		let books = this.props.books;
+
+		let booksArr = books.map(function(item, index) {
+			return (
+				<li className='list__item' key={index}>
+					<i className='list__index'>{index + 1}</i>
+					{item.author} <b>"{item.title}"</b> - {item.year}г ({item.pages} стр)
+					<span className='list__buttons'>
+						<button className='button button--green'>Редактировать</button>
+						<button className='button button--red'>Удалить</button>
+					</span>
+				</li>
+			);
+		});
+
 		return (
 			<div className='book-list'>
 				<h2>Список книг</h2>
 				<div className='book-list__table'>
 					<ol className='list'>
-					<li className='list__item'>
-						<i className='list__index'>1</i>
-						Автор <b>"Заголовок"</b> - 2222г (3333 стр)
-						<span className='list__buttons'>
-							<button onClick={this.edit} className='button button--green'>Редактировать</button>
-							<button onClick={this.delete} className='button button--red'>Удалить</button>
-						</span>
-					</li>
+						{booksArr}
 					</ol>
 					<p><strong>Добавлено книг: 0</strong></p>
 				</div>
