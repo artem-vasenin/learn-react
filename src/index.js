2cf1f8d61543faf423502464be2747865026bcc0
DIR\artem.vasenin <artem.vasenin@accenture.com>
2017-09-26 11:13:38 +0300

    Отступы

--- src/index.js
+++ src/index.js
@@ -6,42 +6,42 @@ import './css/style.css';
 const container = document.getElementById('root');
 
 const Library = React.createClass({
-	getInitialState: function(){
+	getInitialState: function () {
 		return {
 			data: [],
 			update: false
 		};
 	},
-	handlleSaveBook: function(itemObj, index) {
-		const tmpState = this.state,
-					booksArr = tmpState.data;
+	handlleSaveBook: function (itemObj, index) {
+		const tmpState = JSON.parse(JSON.stringify(this.state)),
+			booksArr = tmpState.data;
 
-		if(index === undefined)
+		if (index === undefined)
 			booksArr.push(itemObj);
 		else
 			this.handlleUpdate(index, itemObj);
 
-    this.setState({data: booksArr});
-    this.handlleUpdate(false, false, 0);
+		this.setState({ data: booksArr });
+		this.handlleUpdate(false, false, 0);
 	},
-	handlleUpdate: function(index, itemObj, flag) { 
-    if(flag === 0){
-      this.setState({update: false});
-		}else{
-			this.setState({update: {index: index, item: itemObj}});
-    }
+	handlleUpdate: function (index, itemObj, flag) {
+		if (flag === 0) {
+			this.setState({ update: false });
+		} else {
+			this.setState({ update: { index: index, item: itemObj } });
+		}
 	},
-	handlleDeleteBook: function(index) {
-		const	tmpState = this.state,
-					booksArr = tmpState.data;
-    booksArr.splice(index, 1);
-		this.setState({data: booksArr});
+	handlleDeleteBook: function (index) {
+		const tmpState = JSON.parse(JSON.stringify(this.state)),
+			booksArr = tmpState.data;
+		booksArr.splice(index, 1);
+		this.setState({ data: booksArr });
 	},
-	render: function(){
+	render: function () {
 		return (
 			<div className='library'>
 				<h1>Библиотека</h1>
-				<AddForm save={this.handlleSaveBook} update={this.state.update}  />
+				<AddForm save={this.handlleSaveBook} update={this.state.update} />
 				<BookList books={this.state.data} update={this.handlleUpdate} delete={this.handlleDeleteBook} />
 			</div>
 		);
@@ -49,75 +49,75 @@ const Library = React.createClass({
 });
 
 const AddForm = React.createClass({
-  shouldComponentUpdate(nextProps, nextState){
-		if(nextProps.update){
+	shouldComponentUpdate(nextProps, nextState) {
+		if (nextProps.update) {
 			nextState.update = nextProps.update;
-			nextState.title	 = nextProps.update.item.title;
+			nextState.title = nextProps.update.item.title;
 			nextState.author = nextProps.update.item.author;
-			nextState.year	 = nextProps.update.item.year;
-			nextState.pages	 = nextProps.update.item.pages;
-			nextState.desc	 = nextProps.update.item.desc;
-    }
+			nextState.year = nextProps.update.item.year;
+			nextState.pages = nextProps.update.item.pages;
+			nextState.desc = nextProps.update.item.desc;
+		}
 		return true;
-  },
-	getInitialState: function() {
+	},
+	getInitialState: function () {
 		return {
-      submit:  ' disabled',
-      update: this.props.update,
-      title:   '',
-      author:  '',
-      year:    '',
-      pages:   '',
-      desc:    ''
+			submit: ' disabled',
+			update: this.props.update,
+			title: '',
+			author: '',
+			year: '',
+			pages: '',
+			desc: ''
 		};
 	},
-	handlleSaveClick: function(e) {
+	handlleSaveClick: function (e) {
 		e.preventDefault();
-		
-    let title = this.state.title,
-      author 	= this.state.author,
-      year 	  = this.state.year,
-      pages 	= this.state.pages,
-      desc 	  = this.state.desc;
-		
-		if(this.state.submit === ' disabled' || (!title && !author)) return;
 
-		if(this.state.update)
+		let title = this.state.title,
+			author = this.state.author,
+			year = this.state.year,
+			pages = this.state.pages,
+			desc = this.state.desc;
+
+		if (this.state.submit === ' disabled' || (!title && !author)) return;
+
+		if (this.state.update)
 			this.props.save(this.state.update.item, this.state.update.index);
 		else
-			this.props.save({title: title, author: author, year: year || '---', pages: pages || '---', desc: desc || '---'});
-    
-    this.setState({update: false}),
-    this.setState({title:  ''}),
-    this.setState({author: ''}),
-    this.setState({year:   ''}),
-    this.setState({pages:  ''}),
-    this.setState({desc:   ''});
-		this.setState({submit: ' disabled'});
-  },
-  handlleFieldChange: function(e){
-    const value = e.target.value.trim(),
-          name  = e.target.name;
-          
-    switch(name){
-      case 'title':   this.setState({title: value});  break;
-      case 'author':  this.setState({author: value}); break;
-      case 'year':    this.setState({year: value});   break;
-      case 'pages':   this.setState({pages: value});  break;
-      case 'desc':    this.setState({desc: value});   break;
-    }
-  },
-  handlleFieldBlur: function(){
-    if(this.state.title.trim() && this.state.author.trim()){
-      this.refs.submit.disabled = false;
-      this.setState({submit: ''});
-    }
-    else{
-      this.refs.submit.disabled = true;
-      this.setState({submit: ' disabled'});
-    }
-  },
-	render: function(){
+			this.props.save({ title: title, author: author, year: year || '---', pages: pages || '---', desc: desc || '---' });
+
+		this.setState({ update: false }),
+			this.setState({ title: '' }),
+			this.setState({ author: '' }),
+			this.setState({ year: '' }),
+			this.setState({ pages: '' }),
+			this.setState({ desc: '' });
+		this.setState({ submit: ' disabled' });
+	},
+	handlleFieldChange: function (e) {
+		const value = e.target.value.trim(),
+			name = e.target.name;
+
+		switch (name) {
+			case 'title': this.setState({ title: value }); break;
+			case 'author': this.setState({ author: value }); break;
+			case 'year': this.setState({ year: value }); break;
+			case 'pages': this.setState({ pages: value }); break;
+			case 'desc': this.setState({ desc: value }); break;
+		}
+	},
+	handlleFieldBlur: function () {
+		if (this.state.title.trim() && this.state.author.trim()) {
+			this.refs.submit.disabled = false;
+			this.setState({ submit: '' });
+		}
+		else {
+			this.refs.submit.disabled = true;
+			this.setState({ submit: ' disabled' });
+		}
+	},
+	render: function () {
 		return (
 			<form className='add-form'>
 				<label className='label'>
@@ -149,12 +149,12 @@ const AddForm = React.createClass({
 });
 
 const BookList = React.createClass({
-	render: function(){
+	render: function () {
 		const books = this.props.books,
-			update 	  = this.props.update,
-			del 	    = this.props.delete;
+			update = this.props.update,
+			del = this.props.delete;
 
-		let booksArr = books.map(function(item, index) {
+		let booksArr = books.map(function (item, index) {
 			return (
 				<Book item={item} count={index} key={index} index={index} update={update} delete={del} />
 			);
@@ -175,27 +175,27 @@ const BookList = React.createClass({
 });
 
 const Book = React.createClass({
-	getInitialState: function() {
+	getInitialState: function () {
 		return {
-      description: ' hide',
-      update: false
+			description: ' hide',
+			update: false
 		};
 	},
-	handlleReadmore: function(e) {
+	handlleReadmore: function (e) {
 		e.preventDefault();
-		if(this.state.description)
-			this.setState({description: ''});
+		if (this.state.description)
+			this.setState({ description: '' });
 		else
-			this.setState({description: ' hide'});
+			this.setState({ description: ' hide' });
 	},
-	handlleDelete: function(){
+	handlleDelete: function () {
 		this.props.delete(this.props.index);
 	},
-	handlleUpdate: function(){
-    this.props.update(this.props.index, this.props.item);
-    this.setState({update: true});
+	handlleUpdate: function () {
+		this.props.update(this.props.index, this.props.item);
+		this.setState({ update: true });
 	},
-	render: function(){
+	render: function () {
 		const item = this.props.item,
 			count = this.props.count;
 
