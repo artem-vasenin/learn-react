4c6439271c63e77b2ebfbf12eec2c176528b76de
DIR\artem.vasenin <artem.vasenin@accenture.com>
2017-09-27 09:08:01 +0300

    Поправил опечатку handle

--- src/index.js
+++ src/index.js
@@ -12,19 +12,19 @@ const Library = React.createClass({
 			update: false
 		};
 	},
-	handlleSaveBook: function (itemObj, index) {
+	handleSaveBook: function (itemObj, index) {
 		const tmpState = Object.assign({}, this.state);
 		
 		if (index === undefined){
 			tmpState.data.push(itemObj);
 		} else {
-			this.handlleUpdate(index, itemObj);
+			this.handleUpdate(index, itemObj);
 		}
 
 		this.setState({data: tmpState});
-		this.handlleUpdate(false, false, true);
+		this.handleUpdate(false, false, true);
 	},
-	handlleUpdate: function (index, itemObj, clearUpdateState) {		
+	handleUpdate: function (index, itemObj, clearUpdateState) {		
 		let tmpState = Object.assign({}, this.state);
 				
 		if (clearUpdateState) {
@@ -35,7 +35,7 @@ const Library = React.createClass({
 		}
 		this.setState(tmpState);
 	},
-	handlleDeleteBook: function (index) {
+	handleDeleteBook: function (index) {
 		const tmpState = Object.assign({}, this.state);
 		tmpState.data.splice(index, 1);
 		this.setState(tmpState);
@@ -45,13 +45,13 @@ const Library = React.createClass({
 			<div className='library'>
 				<h1>Библиотека</h1>
 				<AddForm 
-					save={this.handlleSaveBook} 
+					save={this.handleSaveBook} 
 					update={this.state.update} 
 				/>
 				<BookList 
 					books={this.state.data} 
-					update={this.handlleUpdate} 
-					delete={this.handlleDeleteBook} 
+					update={this.handleUpdate} 
+					delete={this.handleDeleteBook} 
 				/>
 			</div>
 		);
@@ -86,7 +86,7 @@ const AddForm = React.createClass({
 			desc: ''
 		};
 	},
-	handlleSaveClick: function (e) {
+	handleSaveClick: function (e) {
 		e.preventDefault();
 
 		let item = {
@@ -98,8 +98,6 @@ const AddForm = React.createClass({
 		};
 
 		if (this.state.submit === ' disabled' || (!item.title && !item.author)) return;
-
-		console.log(this.state.update.item);
 
 		if (this.state.update){
 			this.props.save(item, this.state.update.index);
@@ -117,7 +115,7 @@ const AddForm = React.createClass({
 			submit: ' disabled'
 		});
 	},
-	handlleFieldChange: function (e) {
+	handleFieldChange: function (e) {
 		const value = e.target.value,
 			name = e.target.name;
 
@@ -129,7 +127,7 @@ const AddForm = React.createClass({
 			case 'desc': this.setState({desc: value}); break;
 		}
 	},
-	handlleFieldBlur: function () {
+	handleFieldBlur: function () {
 		if (this.state.title.trim() && this.state.author.trim()) {
 			this.setState({submit: ''});
 		} else {
@@ -144,8 +142,8 @@ const AddForm = React.createClass({
 					<input 
 						className='textfield textfield--title' 
 						name='title' 
-						onBlur={this.handlleFieldBlur} 
-						onChange={this.handlleFieldChange}
+						onBlur={this.handleFieldBlur} 
+						onChange={this.handleFieldChange}
 						value={this.state.title}
 						placeholder='Обязательно для заполнения' 
 						autoFocus
@@ -156,8 +154,8 @@ const AddForm = React.createClass({
 					<input 
 						className='textfield textfield--author' 
 						name='author' 
-						onBlur={this.handlleFieldBlur} 
-						onChange={this.handlleFieldChange}
+						onBlur={this.handleFieldBlur} 
+						onChange={this.handleFieldChange}
 						value={this.state.author}
 						placeholder='Обязательно для заполнения'
 					/>
@@ -167,8 +165,8 @@ const AddForm = React.createClass({
 					<input 
 						className='textfield textfield--year' 
 						name='year' 
-						onBlur={this.handlleFieldBlur} 
-						onChange={this.handlleFieldChange}
+						onBlur={this.handleFieldBlur} 
+						onChange={this.handleFieldChange}
 						value={this.state.year}
 					/>
 				</label>
@@ -177,8 +175,8 @@ const AddForm = React.createClass({
 					<input 
 						className='textfield textfield--pages' 
 						name='pages' 
-						onBlur={this.handlleFieldBlur} 
-						onChange={this.handlleFieldChange}
+						onBlur={this.handleFieldBlur} 
+						onChange={this.handleFieldChange}
 						value={this.state.pages} 
 					/>
 				</label>
@@ -187,15 +185,15 @@ const AddForm = React.createClass({
 					<textarea 
 						className='textfield textfield--desc' 
 						name='desc' 
-						onBlur={this.handlleFieldBlur} 
-						onChange={this.handlleFieldChange}
+						onBlur={this.handleFieldBlur} 
+						onChange={this.handleFieldChange}
 						value={this.state.desc}
 					></textarea>
 				</label>
 				<div className='button-block'>
 					<button 
 						className={'button' + this.state.submit} 
-						onClick={this.handlleSaveClick}
+						onClick={this.handleSaveClick}
 					>
 						{!this.props.update ? 'Добавить книгу' : 'Редактировать'}
 					</button>
@@ -245,7 +243,7 @@ const Book = React.createClass({
 			update: false
 		};
 	},
-	handlleReadmore: function (e) {
+	handleReadmore: function (e) {
 		e.preventDefault();
 
 		if (this.state.description)
@@ -253,10 +251,10 @@ const Book = React.createClass({
 		else
 			this.setState({description: ' hide'});
 	},
-	handlleDelete: function () {
+	handleDelete: function () {
 		this.props.delete(this.props.index);
 	},
-	handlleUpdate: function () {
+	handleUpdate: function () {
 		this.props.update(this.props.index, this.props.item);
 		this.setState({update: true});
 	},
@@ -266,7 +264,7 @@ const Book = React.createClass({
 
 		return (
 			<li className='list__item'>
-				<span className='list__info' onClick={this.handlleReadmore}>
+				<span className='list__info' onClick={this.handleReadmore}>
 					<i className='list__index'>{count + 1}</i>
 					{item.author} 
 					<b>"{item.title}"</b> - 
@@ -275,8 +273,8 @@ const Book = React.createClass({
 				</span>
 				<p className={'desc' + this.state.description}>{item.desc || 'Описание отсутствует'}</p>
 				<span className='list__buttons'>
-					<button className='button button--green' onClick={this.handlleUpdate} >&#9998;</button>
-					<button className='button button--red' onClick={this.handlleDelete}>&otimes;</button>
+					<button className='button button--green' onClick={this.handleUpdate} >&#9998;</button>
+					<button className='button button--red' onClick={this.handleDelete}>&otimes;</button>
 				</span>
 			</li>
 		);
