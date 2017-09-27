3366e5e4481db31c239ca9fc28c6fc5603bd6cca
DIR\artem.vasenin <artem.vasenin@accenture.com>
2017-09-27 18:03:33 +0300

    Создаю actions, reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,19 @@
-export default function actions(){
-    return 'actions';
+// типы действий
+
+export const ADD_BOOK = 'ADD_BOOK';
+export const DELETE_BOOK = 'DELETE_BOOK';
+export const UPDATE_BOOK = 'UPDATE_BOOK';
+
+// генераторы действий
+
+export function addBook(bookInfo){
+    return {type: ADD_BOOK, bookInfo};
+}
+
+export function deleteBook(bookIndex){
+    return {type: DELETE_BOOK, bookIndex};
+}
+
+export function updateBook(bookObject){
+    return {type: ADD_BOOK, bookObject};
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,27 @@
-export default function reducer(){
-    return 'reducer';
+import Redux from 'redux';
+import {addBook, deleteBook, updateBook} from '../actions'
+
+const initialState = {
+    data: [],
+    update: false
+};
+
+function addBookReducer(state = initialState, action){
+    switch(action.type){
+        case ADD_BOOK: return Object.assign({}, state, {
+            data: [
+                ...state.data,
+                {
+                    title: action.book.title,
+                    author: action.book.author,
+                    year: action.book.year,
+                    pages: action.book.pages,
+                    desc: action.book.desc
+                }
+            ]
+        });
+        default: return state;
+    }
 }
+
+//https://rajdee.gitbooks.io/redux-in-russian/content/docs/basics/Reducers.html
