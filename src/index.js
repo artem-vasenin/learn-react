748d4fe8e8f6402400f7b2200b199831c3f090b6
DIR\artem.vasenin <artem.vasenin@accenture.com>
2017-10-03 13:15:28 +0300

    Попытка исправить ошибку доступа к файлам

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import store from './store';
+import {store} from './store/store';
 import Library from './components/Library';
 
 import './css/style.css';
